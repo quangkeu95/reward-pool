e9cd772ce20c285e72539264f9a0f4bd98342cc4
quangkeu95 <quangkeu95@gmail.com>
2025-08-14 10:15:20 +0700

    chore: Add example

---
+++ create-farm.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cargo run init --staking-mint 3fRXYxvUkgdMB3NtiSWifnbDN5dTacofgb6s5QaYHsYu --reward-a-mint RoamA1USA8xjvpTJZ6RvvxyDRzNh6GCA1zVGKSiMVkn --reward-b-mint RoamA1USA8xjvpTJZ6RvvxyDRzNh6GCA1zVGKSiMVkn 2419200 --cluster mainnet --wallet-path ./keypair.json --base ./keypair.json
+
